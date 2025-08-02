34913bcbcb30b1a1c59fd456a9c5ae8b53e12027
Dheerajkumar Solanki <dheerajkumar.solanki@one.app>
2025-08-02 20:39:34 +0530

    ollama compatibility added

--- cohorts/2025/0a-agents/dks_chat_assistent.py
+++ cohorts/2025/0a-agents/dks_chat_assistent.py
@@ -1,8 +1,114 @@
 
 import json
-
+from typing import Any
+import ollama
+import openai
 from IPython.display import display, HTML
 import markdown
+
+
+class LLM:
+    def __init__(self, llm_model: str):
+        self.llm_model = llm_model
+
+    def get_client(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def extract_response(self, llm_response: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def get_llm_type(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def get_llm_model(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def decide_function_call(self, llm_response_entity: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def extract_function_call(self, llm_response_entity: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def decide_message(self, llm_response_entity: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def extract_message(self, llm_response_entity: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def function_call_output(self, tool_call_response: Any, result: Any):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class OllamaLLM(LLM):
+    def __init__(self, llm_model: str):
+        super().__init__(llm_model)
+
+    def get_client(self):
+        return ollama.Client()
+
+    def extract_response(self, llm_response: Any):
+        return [llm_response.message]
+
+    def get_llm_type(self):
+        return "ollama"
+
+    def get_llm_model(self):
+        return self.llm_model
+
+    def decide_function_call(self, llm_response_entity: Any):
+        return llm_response_entity.tool_calls is not None
+
+    def extract_function_call(self, llm_response_entity: Any):
+        return llm_response_entity.tool_calls[0].function
+
+    def decide_message(self, llm_response_entity: Any):
+        return llm_response_entity.content != ""
+
+    def extract_message(self, llm_response_entity: Any):
+        return llm_response_entity.content
+
+    def function_call_output(self, tool_call_response: Any, result):
+        return {
+            "role": "tool",
+            "content": json.dumps(result, indent=2),
+            "tool_name": tool_call_response.name,
+        }
+
+
+class GPTLLM(LLM):
+    def __init__(self, llm_model: str):
+        super().__init__(llm_model)
+
+    def get_client(self):
+        return openai.OpenAI()
+
+    def extract_response(self, llm_response: Any):
+        return llm_response.output
+
+    def get_llm_type(self):
+        return "gpt"
+
+    def get_llm_model(self):
+        return self.llm_model
+
+    def decide_function_call(self, llm_response_entity: Any):
+        return llm_response_entity.type == "function_call"
+
+    def extract_function_call(self, llm_response_entity: Any):
+        return llm_response_entity
+
+    def decide_message(self, llm_response_entity: Any):
+        return llm_response_entity.type == "message"
+
+    def extract_message(self, llm_response_entity: Any):
+        return llm_response_entity.content[0].text
+
+    def function_call_output(self, tool_call_response: Any, result: Any):
+        return {
+            "type": "function_call_output",
+            "call_id": tool_call_response.call_id,
+            "output": json.dumps(result, indent=2),
+        }
 
 
 class Tools:
@@ -14,21 +120,25 @@ class Tools:
         self.tools[function.__name__] = description
         self.functions[function.__name__] = function
 
-    def get_tools(self):
-        return list(self.tools.values())
+    def get_tools(self, llm_type: str = "gpt"):
+        if llm_type == "gpt":
+            return list(self.tools.values())
+        elif llm_type == "ollama":
+            return list(self.functions.values())
+        else:
+            raise ValueError(f"Unsupported LLM type: {llm_type}")
 
-    def function_call(self, tool_call_response):
+    def function_call(self, tool_call_response, llm_object: LLM):
         function_name = tool_call_response.name
-        arguments = json.loads(tool_call_response.arguments)
+        if isinstance(tool_call_response.arguments, dict):
+            arguments = tool_call_response.arguments
+        else:
+            arguments = json.loads(tool_call_response.arguments)
 
         f = self.functions[function_name]
         result = f(**arguments)
 
-        return {
-            "type": "function_call_output",
-            "call_id": tool_call_response.call_id,
-            "output": json.dumps(result, indent=2),
-        }
+        return llm_object.function_call_output(tool_call_response, result)
 
 
 def shorten(text, max_length=50):
@@ -57,7 +167,7 @@ class ChatInterface:
             </div>
             <div>
                 <b>Output</b>
-                <pre>{result['output']}</pre>
+                <pre>{result}</pre>
             </div>
 
             </details>
@@ -65,7 +175,7 @@ class ChatInterface:
         display(HTML(call_html))
 
     def display_response(self, entry):
-        response_html = markdown.markdown(entry.content[0].text)
+        response_html = markdown.markdown(entry)
         html = f"""
             <div>
                 <div><b>Assistant:</b></div>
@@ -77,35 +187,35 @@ class ChatInterface:
 
 class ChatAssistant:
     def __init__(self, tools, developer_prompt, chat_interface, client,
-                 llm_type: str = "gpt", llm_model: str = "gpt-4o-mini"):
+                 llm: LLM):
         self.tools = tools
         self.developer_prompt = developer_prompt
         self.chat_interface = chat_interface
         self.client = client
-        self.llm_type = llm_type
-        self.llm_model = llm_model
+        self.llm = llm
 
     def gpt(self, chat_messages):
         return self.client.responses.create(
-            model=self.llm_model,
+            model=self.llm.get_llm_model(),
             input=chat_messages,
             tools=self.tools.get_tools(),
         )
     
     def ollama(self, chat_messages):
-        return self.client.responses.create(
-            model=self.llm_model,
-            input=chat_messages,
-            tools=self.tools.get_tools(),
+        return self.client.chat(
+            model=self.llm.get_llm_model(),
+            messages=chat_messages,
+            tools=self.tools.get_tools(llm_type="ollama"),
+            think=False,
         )
 
     def get_llm_response(self, chat_messages: list[dict]):
-        if self.llm_type == "gpt":
+        if self.llm.get_llm_type() == "gpt":
             return self.gpt(chat_messages)
-        elif self.llm_type == "ollama":
+        elif self.llm.get_llm_type() == "ollama":
             return self.ollama(chat_messages)
         else:
-            raise ValueError(f"Unsupported LLM type: {self.llm_type}")
+            raise ValueError(f"Unsupported LLM type: {self.llm.get_llm_type()}")
 
     def run(self):
         chat_messages = [
@@ -127,18 +237,20 @@ class ChatAssistant:
 
                 has_messages = False
 
-                for entry in response.output:
+                for entry in self.llm.extract_response(response):
                     chat_messages.append(entry)
 
-                    if entry.type == "function_call":
-                        result = self.tools.function_call(entry)
+                    if self.llm.decide_function_call(entry):
+                        function_call_entity = self.llm.extract_function_call(entry)
+                        result = self.tools.function_call(function_call_entity)
                         chat_messages.append(result)
                         self.chat_interface.display_function_call(
-                            entry, result
+                            function_call_entity, result
                         )
 
-                    elif entry.type == "message":
-                        self.chat_interface.display_response(entry)
+                    elif self.llm.decide_message(entry):
+                        message_entity = self.llm.extract_message(entry)
+                        self.chat_interface.display_response(message_entity)
                         has_messages = True
 
                 if has_messages:
